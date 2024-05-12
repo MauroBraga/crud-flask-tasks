d34f31524ee2f88ecf1870eb648ad08e69dd25fa
mauro <mrb0305@gmail.com>
2024-05-12 15:23:30 -0300

    crud find all

--- app.py
+++ app.py
@@ -18,5 +18,19 @@ def create_task():
   return jsonify({"message": "Nova tarefa criada com sucesso"})
 
 
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+  task_list=[task.to_dict() for task in tasks]
+#  for task in tasks:
+#    task_list.append(task.to_dict())
+
+  output = {
+    "tasks":task_list,
+    "total_tasks":0
+  }
+
+  return jsonify(task_list)
+
+
 if __name__ == "__main__":
   app.run(debug=True)
